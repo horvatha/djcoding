6d62e749c4fc8acc602254d590e6f2753fb28bb0
Arpad Horvath <horvath.arpad.szfvar@gmail.com>
2014-10-09 23:30:42 +0200

    functional_tests/tests/test_arithmetic.py added

---
+++ functional_tests/tests/test_arithmetic.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+"""functional tests for the arithmetic module
+"""
+
+from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+import unittest
+from django.test import LiveServerTestCase
+import time
+
+
+def sleep_a_bit():
+    time.sleep(0)
+
+
+class MyLiveServerTestCase(LiveServerTestCase):
+
+    def assertAreNotInPage(self, text_list):
+        for text in text_list:
+            self.assertNotIn(text, self.browser.page_source)
+
+    def assertAreInPage(self, text_list):
+        for text in text_list:
+            self.assertIn(text, self.browser.page_source)
+
+
+class VisitorTest(MyLiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+        self.server_url = self.live_server_url
+        # self.server_url = "http://django.amk.uni-obuda.hu/django"
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_surfing_on_the_page(self):
+        self.browser.get(self.server_url + '/arithmetic/')
+        self.browser.set_window_size(1024, 700)
+
+        coding_link = self.browser.find_element_by_partial_link_text("Kódolás")
+
+        sleep_a_bit()
+        coding_link.click()
+
+        self.assertTrue(self.browser.current_url.endswith('coding/random/'))
+
+        self.browser.get(self.server_url + '/arithmetic/')
+        decoding_link = self.browser.find_element_by_link_text("Dekódolás")
+
+        sleep_a_bit()
+        decoding_link.click()
+
+        self.assertFail("Finish test.")
+
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
